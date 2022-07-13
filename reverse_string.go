8da03833f2955b09bc1dcbb2fc05b80f04aec03c
Aktir <Margarita.odorodko@gmail.com>
2022-07-13 17:09:58 +0300

    commit01

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,10 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	bytes := []rune(input)
+	for i, v := 0, len(bytes)-1; i < v; i, v = i+1, v-1 {
+		bytes[i], bytes[v] = bytes[v], bytes[i]
+	}
+	return string(bytes)
+
 }
